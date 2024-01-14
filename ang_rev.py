085b3f0180807c40f55cd1a610820fa747566571
Willoh Robbins <149435903+willohrobbins@users.noreply.github.com>
2024-01-14 15:59:38 -0600

    Create ang_rev.py

---
+++ ang_rev.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+import time
+
+motorPins = (12, 16, 18, 22)    # define pins connected to four phase ABCD of stepper motor
+CCWStep = (0x01,0x02,0x04,0x08) # define power supply order for rotating anticlockwise
+CWStep = (0x08,0x04,0x02,0x01)  # define power supply order for rotating clockwise
+itC = 0
+
+param_a = -1   # angle change (+ is counterclockwise)
+
+
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering
+    for pin in motorPins:
+        GPIO.setup(pin,GPIO.OUT)
+
+# as for four phase stepping motor, four steps is a cycle. the function is used to drive the stepping motor clockwise or anticlockwise to take four steps
+def moveOnePeriod(direction,ms):
+    for j in range(0,4,1):      # cycle for power supply order
+        for i in range(0,4,1):  # assign to each pin
+            if (direction == 1):# power supply order clockwise
+                GPIO.output(motorPins[i],((CCWStep[j] == 1<<i) and GPIO.HIGH or GPIO.LOW))
+            else :              # power supply order anticlockwise
+                GPIO.output(motorPins[i],((CWStep[j] == 1<<i) and GPIO.HIGH or GPIO.LOW))
+        if(ms<3):       # the delay can not be less than 3ms, otherwise it will exceed speed limit of the motor
+            ms = 3
+        time.sleep(ms*0.001)
+
+# continuous rotation function, the parameter steps specifies the rotation cycles, every four steps is a cycle
+def moveSteps(direction, ms, steps):
+    for i in range(steps):
+        moveOnePeriod(direction, ms)
+
+# function used to stop motor
+def motorStop():
+    for i in range(0,4,1):
+        GPIO.output(motorPins[i],GPIO.LOW)
+
+def loop(itC, param_a):
+    while (itC < 1):
+        if(param_a > 0):
+          moveSteps(1,3, param_a)
+        else:
+          moveSteps(0,3,-1 * param_a)
+        itC += 1
+
+def destroy():
+    GPIO.cleanup()             # Release resource
+
+if __name__ == '__main__':     # Program entrance
+    print ('Program is starting...')
+    setup()
+    try:
+        loop(itC, param_a)
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        destroy()
