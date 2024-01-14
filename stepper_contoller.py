9c5d2bd3ddcfa9d59b6fefad5030bb2adbc7e6e6
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-13 18:28:18 -0600

    Update stepper_contoller.py

--- stepper_contoller.py
+++ stepper_contoller.py
@@ -36,9 +36,9 @@ def motorStop():
 def loop(itC):
     while (itC < 4):
         moveSteps(1,3,100)
-        time.sleep(0.01)
+        time.sleep(0.03)
         moveSteps(0,3,100)
-        time.sleep(0.01)
+        time.sleep(0.03)
         itC += 1
 
 def destroy():
