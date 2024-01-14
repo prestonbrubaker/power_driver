6d0e45485a6b95d7e589c1d38a81cd0e2483e88f
Willoh Robbins <149435903+willohrobbins@users.noreply.github.com>
2024-01-14 15:50:31 -0600

    Update stepper_contoller.py

--- stepper_contoller.py
+++ stepper_contoller.py
@@ -6,9 +6,9 @@ CCWStep = (0x01,0x02,0x04,0x08) # define power supply order for rotating anticlo
 CWStep = (0x08,0x04,0x02,0x01)  # define power supply order for rotating clockwise
 itC = 0
 
-param_a = 20    # maximum angle
-param_b = 7    # time at most clockwise point (on)
-param_c = 7    # time at most counterclockwise point (off)
+param_a = 5    # maximum angle
+param_b = 0    # time at most clockwise point (on)
+param_c = 0    # time at most counterclockwise point (off)
 
 
 def setup():
